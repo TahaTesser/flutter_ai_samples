ff67f1b88ef250aef85b081fd65ddabfc41a8eb4
Taha Tesser <tessertaha@gmail.com>
2024-12-23 23:06:42 +0200

    Update app

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter AI Samples',
       theme: FlexThemeData.light(
         scheme: FlexScheme.brandBlue,
